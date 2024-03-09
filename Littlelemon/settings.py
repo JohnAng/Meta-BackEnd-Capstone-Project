cf6ab53c6c07d5844cf06732ea9773b45d8af9b8
Ioannis Angelakos <angelakos.john@gmail.com>
2024-03-09 22:16:25 +0200

    made Booking and Menu models and added some data

--- Littlelemon/settings.py
+++ Littlelemon/settings.py
@@ -75,13 +75,50 @@ WSGI_APPLICATION = "Littlelemon.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     },
+#     "mydb": {
+#         "ENGINE": "django.db.backends.mysql",
+#         "NAME": "my_database",
+#         "USER": "root",
+#         "PASSWORD": "your_password",
+#         "HOST": "127.0.0.1",
+#         "PORT": "3306",
+#         "OPTIONS": {"init_command": "SET sql_mode='STRICT_TRANS_TABLES'"},
+#     },
+# }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'OPTIONS': {
+#             'read_default_file': '/path/to/my.cnf',
+#         },
+#     }
+# }
+
+
+# # my.cnf
+# [client]
+# 'NAME': 'my_database'
+# 'USER': 'root'
+# 'PASSWORD': 'your_password'
+# default-character-set = utf8
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "LittleLemon",
+        "USER": "root",
+        "PASSWORD": "root",
+        "HOST": "127.0.0.1",
+        "PORT": "3306",
+        "OPTIONS": {"init_command": "SET sql_mode='STRICT_TRANS_TABLES'"},
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Booking(models.Model):
+    name = models.CharField(max_length=255)
+    number_of_guests = models.IntegerField()
+    booking_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.name + "'s " + "reservation"
+
+
+class Menu(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    invantory = models.IntegerField()
+
+    def __str__(self):
+        return self.title
